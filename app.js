6883b881766eb648f31bec57eaa42dd8dcc45440
Mario Alexis Uribe <umario284@gmail.com>
2022-07-26 18:37:11 -0500

    buscador

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+//constantes para adquiriri la informacion de la api
 const API_KEY = 'api_key=2d4cb4615be1b6d3c9183d0b22f9002f';
 const BASE_URL = 'https://api.themoviedb.org/3'
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
-const IMG_URL='https://image.tmdb.org/t/p/w500'
-const main=document.getElementById('main')
+//constante para tener las imagenes 
+const IMG_URL = 'https://image.tmdb.org/t/p/w500'
+//contante para el buscador
+const searchURL = BASE_URL + '/search/movie?'+API_KEY
+
+const main = document.getElementById('main')
+const form=document.getElementById('form')
+const search = document.getElementById('search')
+//const button=document.getElementById('go')
 getMovies(API_URL)
 
 function getMovies(url) {
@@ -11,6 +19,7 @@ function getMovies(url) {
         showMovies(data.results)
     })
 }
+//funcion para mostrar las peliculas 
 function showMovies(data) {
     main.innerHTML = '';
     data.forEach(movie => {
@@ -43,3 +52,14 @@ function getColor(vote) {
         return 'red'
     }
 }
+/* form.addEventListener('onclick',) */
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value
+    if (searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }
+    else {
+        getMovies(API_URL)
+    }
+})
