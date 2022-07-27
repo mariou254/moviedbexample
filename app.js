4d1aa9d0a94391e21709233b8a157af53a7bb74f
Mario Alexis Uribe <umario284@gmail.com>
2022-07-27 16:12:03 -0500

    tags

--- app.js
+++ app.js
@@ -6,11 +6,26 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'
 //contante para el buscador
 const searchURL = BASE_URL + '/search/movie?'+API_KEY
+const genres=[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
 
 const main = document.getElementById('main')
 const form=document.getElementById('form')
 const search = document.getElementById('search')
+const tagsEl=document.getElementById('tags')
 //const button=document.getElementById('go')
+setGenere()
+function setGenere(){
+    tagsEl.innerHTML=''
+    console.log(genres)
+    
+    genres.forEach(genre=>{
+        const t=document.createElement('div')
+        t.classList.add('tag')
+        t.id=genre.id
+        t.innerText=genre.name
+        tagsEl.appendChild(t)
+    })
+}
 getMovies(API_URL)
 
 function getMovies(url) {
